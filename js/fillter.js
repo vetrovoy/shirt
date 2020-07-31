176c4aa78513594fdf58848fd2f769bf4bfc4073
Vetrovoy Andrey <v3trovoy@gmail.com>
2020-07-31 16:22:14 +0300

    Changed

--- js/fillter.js
+++ js/fillter.js
@@ -1,12 +1,3 @@
-//const elem = document.querySelector('.gallary-fillter__items-container');
-////const item = document.querySelectorAll('.gallary-fillter__items-item');
-//
-//const iso = new Isotope( elem, {
-//    
-//  itemSelector: '.gallary-fillter__item',
-//    fillter:'new'
-//});
-
 (function() {
   const elem = document.querySelector(".gallary-fillter__items-container");
   const iso = new Isotope(elem, {
