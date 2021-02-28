e4a84dc9c4aa68a408494a6f6c92f8ba3e12a2cf
vetrovoy <68954442+vetrovoy@users.noreply.github.com>
2021-03-01 00:09:05 +0300

    Update fillter.js

--- js/fillter.js
+++ js/fillter.js
@@ -2,19 +2,22 @@
   const elem = document.querySelector(".gallary-fillter__items-container");
   const iso = new Isotope(elem, {
     itemSelector: ".gallary-fillter__item",
-    filter: '.popular'
+    // filter: '.popular'
   });
 
   const controlls = document.querySelectorAll(".gallery-fillter__link");
+  const mainActiveClass = document.querySelector(".gallery-fillter__nav-active");
   const activeClass = "gallery-fillter__nav-active";
 
   controlls.forEach(function(control) {
+    
     control.addEventListener("click", function(e) {
       e.preventDefault();
       
       const filterName = control.getAttribute("data-filter");
 
       controlls.forEach(function(link) {
+        mainActiveClass.classList.remove(activeClass);
         link.closest(".gallery-fillter__nav").classList.remove(activeClass);
       });
 
@@ -24,5 +27,6 @@
         filter: `.${filterName}`
       })
     });
+
   });
 })();
